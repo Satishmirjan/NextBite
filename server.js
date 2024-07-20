aeae55eb6b2ba2c04cec9e0621af13f0d3fe95b6
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2024-07-20 19:58:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,6 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const app = express();
 
 
-// mongoose.connect('mongodb://localhost:27017/NextBite', { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-
-// const userRoutes = require('./routes/users');
-// const mealRoutes = require('./routes/meals');
-
-// app.use('/api/users', userRoutes);
-// app.use('/api/meals', mealRoutes);
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -31,9 +9,7 @@ const cors = require('cors');
 const app = express();
 
 
-// mongoose.connect('mongodb://localhost:27017/NextBite', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('MongoDB connected'))
-//     .catch(err => console.error('MongoDB connection error:', err));
+
 
 
     mongoose.connect('mongodb://localhost:27017/NextBite')
