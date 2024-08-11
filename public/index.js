4df2e0fc33976c9f374858714f5de437fdc00696
Satish Ishwar Naik <123397923+Satishmirjan@users.noreply.github.com>
2024-08-11 20:33:47 +0530

    Update index.js
    unwanted things has been removed

--- public/index.js
+++ public/index.js
@@ -1,38 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const calendarBody = document.getElementById('calendar-body');
-//   const daysInMonth = new Date(2024, 5, 0).getDate(); 
-//   const firstDay = new Date(2024, 4, 1).getDay(); 
-//   const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-//   let date = 1;
 
-  
-//   const visitedDates = JSON.parse(localStorage.getItem('visitedDates')) || {};
-
-//   for (let i = 0; i < 6; i++) { 
-//       let row = document.createElement('tr');
-      
-//       for (let j = 0; j < 7; j++) {
-//           let cell = document.createElement('td');
-          
-//           if ((i === 0 && j < firstDay) || date > daysInMonth) {
-//               cell.textContent = '';
-//           } else {
-//               cell.textContent = `${weeks[j]} ${date}`;
-//               if (visitedDates[date]) {
-//                   cell.classList.add('visited');
-//               }
-//               cell.addEventListener('click', () => {
-//                   window.location.href = `menu.html?date=${date}`;
-//               });
-//               date++;
-//           }
-          
-//           row.appendChild(cell);
-//       }
-      
-//       calendarBody.appendChild(row);
-//   }
-// });
 
 document.addEventListener('DOMContentLoaded', async () => {
     const calendarBody = document.getElementById('calendar-body');
